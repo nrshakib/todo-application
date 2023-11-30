73dcc971e3999e37d2c272e539c5eaf83d7b6d9d
NRS\nrsha <nrshakib007@gmail.com>
2023-12-01 01:16:14 +0600

    Mapped the ToDos and rendered to the list

---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,16 @@
+import './TodoList.css'
+const TodoList = ({ todos }) => {
+    
+  return (
+    <ul>
+          {todos.map((todo) => {
+          console.log(todo);
+       return <li className="list" key={todo.id}>
+          {todo.name} - {todo.status}
+        </li>;
+      })}
+    </ul>
+  );
+};
+
+export default TodoList;
